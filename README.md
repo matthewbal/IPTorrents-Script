e20059e86d3dded1d4bb1dc56d34ea0df5f12be9
Matt <matt@mbm.space>
2019-04-24 05:56:34 +1000

    check readme changelog

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+Screenshot:
 ![Screenshot](/screenshot.png?raw=true "Screenshot")
+
+version 0.2 (April 2019 updated)
+
+- Used ID instead of class to find table
+- Hide undesirable torrent rows completely
+- Display number of rows hidden
+- First row of torrents is still always shown (avoids confusion)
+
+Reccomended:
+- Change your user account settings to allow 255 (max) torrents on each page.
+
+Happy seeding.
+
+
--- torrent.user.js
+++ torrent.user.js
@@ -1,33 +1,57 @@
 // ==UserScript==
 // @name         torrent
 // @namespace    http://github.com/jacobjiggler
-// @version      0.1
+// @version      0.2
 // @description  Made for ipt-torrents
-// @author       You
+// @author       https://github.com/matthewbal
 // @match        https://ipt-update.com/t*
 // @grant        none
+// lastupdate:   24/04/2019
 // ==/UserScript==
 
-var table = document.getElementsByClassName("torrents")[0];
+// Highly reccomend you go into your user settings and change "torrents per page" to the max of 255.
+// slight bug where the first row is shown regardless of ratio
+// decided this is a feature, not a bug
+
+var table = document.getElementById("torrents");
+
+var allrows = table.rows.length;
+
+badrows = [];
+
 for (var i = 1, row; row = table.rows[i]; i++) {
-   //iterate through rows
-   //rows would be accessed using the "row" variable assigned in the for loop
-   seeders =  Number(row.cells[7].innerHTML);
-   leachers = Number(row.cells[8].innerHTML);
-    size = Number(row.cells[5].innerHTML.substring(0,row.cells[5].innerHTML.length - 3));
-    Gb = (row.cells[5].innerHTML.substring(row.cells[5].innerHTML.length - 2) == 'GB');
-    console.log(Gb);
-    
-    ratio = (leachers/seeders).toFixed(2);
-    if (size < 10 && ratio > .5 && Gb){
+  //iterate through rows
+  //rows would be accessed using the "row" variable assigned in the for loop
+  seeders =  Number(row.cells[7].innerHTML);
+  leachers = Number(row.cells[8].innerHTML);
+  size = Number(row.cells[5].innerHTML.substring(0,row.cells[5].innerHTML.length - 3));
+  Gb = (row.cells[5].innerHTML.substring(row.cells[5].innerHTML.length - 2) == 'GB');
+
+  ratio = (leachers/seeders).toFixed(2);
+  if (size < 10 && ratio > .5 && Gb){
     color = 'green';
-    }
-    else if(size < 80 && ratio > .1 && Gb){
-        color = 'orange';
-    }
-    else {
-     color = 'red';
-    }
-    
-    row.cells[8].innerHTML = (row.cells[8].innerHTML + '<font color="' + color + '"> &nbsp;' +  ratio + '</font>');
+  }
+  else if(size < 80 && ratio > .1 && Gb){
+    color = 'orange';
+    badrows.push(i); //remove this if you want to see "okay" torrents
+  }
+  else {
+    color = 'red';
+    badrows.push(i);
+  }
+
+  row.cells[8].innerHTML = (row.cells[8].innerHTML + '<font color="' + color + '"> &nbsp;' +  ratio + '</font>');
 }
+
+
+//reverse through to maintain structure, delete rows we don't want to see
+for (var i = table.rows.length; i > 0 ; i--){
+  if (i in badrows){
+    table.deleteRow(badrows[i]);
+  }
+}
+
+//display # of torrents hidden above table
+var info = document.getElementsByClassName("info")[0];
+var hidden = badrows.length;
+info.innerHTML +="We hid " + hidden +"/" + allrows + " torrents";
